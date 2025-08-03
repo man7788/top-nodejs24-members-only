907670060f45c46fad81aa94675978ac30c089e9
man7788 <jerman1841@gmail.com>
2025-08-03 11:05:20 +0800

    create express app files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('node:path');
+
+const indexRouter = require('./routes/indexRouter');
+
+const assetsPath = path.join(__dirname, 'public');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}!`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
