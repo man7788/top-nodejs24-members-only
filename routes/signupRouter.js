6b49a1e6256b31d50c41a60ed5d5f909029d13c2
man7788 <jerman1841@gmail.com>
2025-08-05 12:32:42 +0800

    update sign-up router

--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -4,5 +4,6 @@ const signupController = require('../controllers/signupController');
 const signupRouter = Router();
 
 signupRouter.get('/', signupController.getSignup);
+signupRouter.post('/', signupController.postSignup);
 
 module.exports = signupRouter;
