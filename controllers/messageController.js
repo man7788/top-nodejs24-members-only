c8846aa7cc557e385340e8694cb0b3646e5f75ca
man7788 <jerman1841@gmail.com>
2025-08-08 02:45:38 +0800

    create message controller

---
+++ controllers/messageController.js
@@ -0,0 +1,27 @@
+const db = require('../db/queries');
+const { body, validationResult } = require('express-validator');
+
+const messageErr = 'must be between 1 and 1000 characters.';
+
+const validateUser = [
+  body('message')
+    .trim()
+    .isLength({ min: 1, max: 1024 })
+    .withMessage(`Message ${messageErr}`)
+    .escape(),
+];
+
+exports.postMessage = [
+  validateUser,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.flash('messages', errors.array());
+      return res.redirect('/members-only');
+    }
+
+    await db.createMessage(req.session.passport.user, req.body.message);
+    res.redirect('/members-only');
+  },
+];
