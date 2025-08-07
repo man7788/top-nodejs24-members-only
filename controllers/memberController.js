fba2db5ad399f40447672f324015d1c951e481ef
man7788 <jerman1841@gmail.com>
2025-08-07 09:43:40 +0800

    create post secret code controller

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,3 +1,43 @@
+const db = require('../db/queries');
+const { body, validationResult } = require('express-validator');
+
+const codeErr = 'must be between 1 and 64 characters.';
+
+const validateSecret = [
+  body('passcode')
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage(`Secret code ${codeErr}`)
+    .escape(),
+];
+
 exports.getMember = (req, res) => {
   res.render('member', { title: 'Welcome to Members Only' });
 };
+
+exports.postSecretCode = [
+  validateSecret,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render('member', {
+        title: 'Welcome to Members Only',
+        errors: errors.array(),
+      });
+    }
+
+    const result = await db.readSecretCode();
+
+    if (result[0].secret_code !== req.body.passcode) {
+      return res.render('member', {
+        title: 'Welcome to Members Only',
+        errors: [{ msg: 'Secret passcode is invalid' }],
+      });
+    }
+
+    await db.updateUserById(req.session.passport.user);
+
+    res.redirect('/members-only');
+  },
+];
